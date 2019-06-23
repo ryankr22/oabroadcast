a83d14bbf2a0d5464ae5c74b2863173f2fd7dac3
Ryan <james.70@wright.edu>
2019-06-23 12:35:41 -0400

    added build to git

---
+++ client/build/precache-manifest.c3a176c455e93569cb26e63b702a2ddc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e9cae8c69b6904c4f80b",
+    "url": "/static/js/main.56e57bbd.chunk.js"
+  },
+  {
+    "revision": "9930496da6b76c0467c4",
+    "url": "/static/js/2.3a34bf5a.chunk.js"
+  },
+  {
+    "revision": "e9cae8c69b6904c4f80b",
+    "url": "/static/css/main.b889874b.chunk.css"
+  },
+  {
+    "revision": "9930496da6b76c0467c4",
+    "url": "/static/css/2.0b871449.chunk.css"
+  },
+  {
+    "revision": "ac8d972fd0f03808737abeee886d9f62",
+    "url": "/index.html"
+  }
+];
